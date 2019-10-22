007735d6ee707698cca2fc3228f397610feea49c
valentin <valetobares11@gmail.com>
2019-10-22 09:21:59 -0300

    conteiner color de reset pass

--- triviavet/client/screens/ResetPass.js
+++ triviavet/client/screens/ResetPass.js
@@ -41,15 +41,15 @@ export default class ResetPass extends React.Component {
          <View style={{margin:20}} />
 
         <Button title="Enviar" onPress={this._sendUsername}
-          color = "#ebee2c"
+          color = "#d35400"
         />
          <View style={{margin:20}} />
         <Button title="Ya tengo codigo" onPress={() => this.props.navigation.navigate('InsertCode')}
-          color = "#ebee2c"
+          color = "#d35400"
         />
         <View style={{margin:20}} />
        <Button title="Volver" onPress={() => this.props.navigation.navigate('Auth')}
-         color = "#ebee2c"
+         color = "#d35400"
        />
       </View>
     );
@@ -68,7 +68,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: '#48c9b0',
+    backgroundColor: '#1b4f72',
   },
   text: {
     fontSize: 20,
