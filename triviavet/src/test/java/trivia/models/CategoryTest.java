7412b4def5c51b2f62c8eb5cd9fc407a9e636022
Agustin <agustinborda1998@gmail.com>
2019-11-04 03:28:26 -0300

    [#168818272] added tests for Category model

--- triviavet/src/test/java/trivia/models/CategoryTest.java
+++ triviavet/src/test/java/trivia/models/CategoryTest.java
@@ -1,26 +1,27 @@
 package trivia.models;
 
-import org.javalite.activejdbc.Base;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import controllers.CategoryController;
+import trivia.testUtils.DBSpecAlternative;
+
 import static org.junit.Assert.*;
 
-public class CategoryTest {
+public class CategoryTest extends DBSpecAlternative{
   
   @Before
   public void before(){
-    Base.open();
     System.out.println("Category test setup");
-    Base.openTransaction();
+    CategoryController.createCategory("test");
   }
 
   @After
   public void after(){
     System.out.println("Category test tearDown");
-    Base.rollbackTransaction();
-    Base.close();
+
   }
 
   @Test
@@ -33,15 +34,28 @@ public class CategoryTest {
   @Test
   public void validatePrecenseOfSomethingInNombre(){
     Category categoryTest = new Category();
-    categoryTest.set("nombre", "fisica");
+    categoryTest.setNombre("test2");
     assertEquals(categoryTest.isValid(), true);
   }
 
   @Test
   public void validateUniqueCategory(){
-    assertTrue(new Category().set("nombre", "fisica").saveIt());
     Category categoryTest = new Category();
-    categoryTest.set("nombre", "fisica");
+    categoryTest.setNombre("test");
     assertEquals(categoryTest.isValid(), false);
   }
+  
+  @Test
+  public void getNombreTest() {
+	  Category cat = Category.findFirst("nombre = ?", "test");
+	  assertEquals(cat.getString("nombre"), cat.getNombre());
+  }
+  
+  @Test
+  public void setNombreTest() {
+	  Category cat = Category.findFirst("nombre = ?", "test");
+	  cat.setNombre("modificado");
+	  assertEquals(cat.getNombre(), "modificado");
+  }
+  
 }
