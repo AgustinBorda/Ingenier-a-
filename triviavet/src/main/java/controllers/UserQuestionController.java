44824843cb90b5d074b9648049e58546610b0e65
Agustin <agustinborda1998@gmail.com>
2019-11-04 19:51:57 -0300

    [Finishes #168818272] added more tests

--- triviavet/src/main/java/controllers/UserQuestionController.java
+++ triviavet/src/main/java/controllers/UserQuestionController.java
@@ -2,9 +2,15 @@ package controllers;
 
 import trivia.models.User;
 import trivia.models.UserQuestion;
-
+/**
+ * Class that provides a controller for the UserQuestion model.
+ */
 public class UserQuestionController {
-
+	/**
+	 * Create a user question.
+	 * @param username the username of a user.
+	 * @param preg_id the id of a question.
+	 */
 	public static void createUserQuestion(String username,Object preg_id) {
 		UserQuestion preg = new UserQuestion();
 		User u = User.findFirst("username = ?", username);
